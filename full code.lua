3a3d71375ef7a48101a0257b55ccee8645727c27
D4rk9690 <83451533+D4rk9690@users.noreply.github.com>
2021-05-16 20:25:54 +0200

    Update full code.lua

--- full code.lua
+++ full code.lua
@@ -77,7 +77,7 @@ checkcode.TextScaled = true
 checkcode.TextSize = 14.000
 checkcode.TextWrapped = true
 checkcode.MouseButton1Down:connect(function()
-	if codehere.Text == "D4rkwin" then
+	if codehere.Text == "D4rkV2" then
 		keyframe.Visible = false
 		print("key is correct! Welcome to D4rk HUB!")
 		
